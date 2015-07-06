b8c64e67e95c82b5a5e69db7f516f6c4601b4847
Colin McNeill <colinpmcneill@gmail.com>
2015-07-06 13:48:09 -0400

    adding hiring thread, regex finder and updated massmail script

---
+++ regex_email_finder.rb
@@ -0,0 +1,6 @@
+f =  File.open("HN_hiring_raw_text.txt")
+content = f.read
+r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
+emails = content.scan(r).uniq
+puts emails
+puts emails.count
